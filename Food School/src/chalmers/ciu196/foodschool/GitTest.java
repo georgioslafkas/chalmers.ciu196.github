855c5a87d8ff998d7f5e76ed93244e4a1b94edc9
georgioslafkas <georgelafkas@gmail.com>
2012-11-30 14:47:00 +0100

    Just another commit test.

--- Food School/src/chalmers/ciu196/foodschool/GitTest.java
+++ Food School/src/chalmers/ciu196/foodschool/GitTest.java
@@ -3,6 +3,6 @@ package chalmers.ciu196.foodschool;
 public class GitTest {
 	public static void main(String args[])
 	{
-		System.out.print("I own you");
+		System.out.print("I own you now!!!");
 	}
 }
