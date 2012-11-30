9ff91c1082fe7b06705fe484ccde0b5881b0d8df
geo <geo@129.16.194.47>
2012-11-30 14:11:41 +0100

    First commit (test)

---
+++ Food School/gen/chalmers/ciu196/foodschool/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package chalmers.ciu196.foodschool;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Food School/src/chalmers/ciu196/foodschool/GitTest.java
@@ -0,0 +1,8 @@
+package chalmers.ciu196.foodschool;
+
+public class GitTest {
+	public static void main(String args[])
+	{
+		System.out.print("I own you");
+	}
+}
