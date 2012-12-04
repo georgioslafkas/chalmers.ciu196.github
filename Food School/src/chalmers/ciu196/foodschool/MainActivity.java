eac10604dee54aee4f5e1b719cec9f667784f20e
georgioslafkas <georgelafkas@gmail.com>
2012-12-04 14:07:02 +0100

    Activity navigation.

--- Food School/src/chalmers/ciu196/foodschool/MainActivity.java
+++ Food School/src/chalmers/ciu196/foodschool/MainActivity.java
@@ -3,7 +3,10 @@ package chalmers.ciu196.foodschool;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	@Override
@@ -28,4 +31,11 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void startLearnActivity(View v)
+	{
+		Intent startLearn = new Intent(this, LearnCategoriesActivity.class);
+		startLearn.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(startLearn);
+		finish();
+	}
 }
