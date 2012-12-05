b4585b0bd2362ed067e688c4e74f166d50d8784a
tasos <anastasios.thomaidis@gmail.com>
2012-12-05 13:22:30 +0100

    small advances on the database part

---
+++ Food School/src/chalmers/ciu196/foodschool/FoodProvider.java
@@ -0,0 +1,12 @@
+package chalmers.ciu196.foodschool;
+
+import android.content.Context;
+
+public class FoodProvider extends DbManager {
+
+	public FoodProvider(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
